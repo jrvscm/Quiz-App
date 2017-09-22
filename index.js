7fadeee54ce3fb9c84c447c246577c5dd1fb9bf9
Chris Jarvis <jrvscm@gmail.com>
2017-09-22 17:43:28 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,342 @@
+let score = [];
+let count = [];
+let questions = [];
+
+function shuffleQuestions() {
+  questionsList.sort(function(a, b) {
+    return Math.floor(Math.random()*3 - 1);
+    });
+  questions = questionsList.slice(0,10);
+}
+
+  
+  function handleStartClicks() {
+    $('.starting-statement-container').off().on('click', '.start-button', event => {
+      console.log('handleStartClicks ran');
+      $('.start-section').fadeOut();
+      $('header').fadeOut();
+      $('.question-answer-section').fadeIn();
+      disableNext();
+      disableSubmit();
+      inputClicked();
+    });  
+  }
+  
+  function renderQuestion(currentCount) {
+    if(currentCount === 10) {
+      restartQuiz(currentCount);
+    } else {
+    let quizQuestion = `<p><em>${questions[currentCount].question}</em></p>`;
+    console.log('renderQuestion ran');
+    selectedAnswers();
+    renderAnswers(quizQuestion, currentCount);
+  }
+  }
+
+  
+  function renderAnswers(quizQuestion, currentCount) {
+    let quizAnswers = [
+         `<div class="answer-div jsAnswerDiv jsAnswerDiv1">
+            <input class="jsChoices" type="radio" id="choice1" name="answer" value="incorrect" required>
+            <label for="choice1">${questions[currentCount].wrongAnswer1}</label>
+          </div>`,
+
+          `<div class="answer-div jsAnswerDiv jsAnswerDiv2">
+            <input class="jsChoices" type="radio" id="choice2" name="answer" value="incorrect" required>
+            <label for="choice2">${questions[currentCount].wrongAnswer2}</label>
+          </div>`,
+
+          `<div class="answer-div jsAnswerDiv jsAnswerDiv3"> 
+            <input class="jsChoices "type="radio" id="choice3" name="answer" value="incorrect" required>
+            <label for="choice3">${questions[currentCount].wrongAnswer3}</label>
+          </div>`,
+
+          `<div class="answer-div jsAnswerDiv jsCorrectChoice jsAnswerDiv4">
+            <input class="jsChoices" type="radio" id="choice4" name="answer" value="correct" required>
+            <label for="choice4">${questions[currentCount].correctAnswer}</label>
+          </div>`];
+
+    console.log('renderAnswers ran');
+    shuffleAnswers(quizAnswers, currentCount);
+    insertNewQuestion(quizQuestion, currentCount);
+  }
+
+  function shuffleAnswers(quizAnswers, currentCount) {
+    quizAnswers = quizAnswers.sort(function(a, b) {
+      return Math.floor(Math.random()*3);
+    });
+      quizAnswers = quizAnswers.join('');
+    insertNewAnswer(quizAnswers, currentCount);
+    console.log('shuffleAnswers ran');
+  }
+
+  function insertNewQuestion(quizQuestion, currentCount) {
+    $('.quiz-question p').fadeOut()
+    setTimeout(function() {
+    $('.quiz-question').empty().append(quizQuestion).fadeIn();
+  }, 350);
+    console.log('insertNewQuestion ran');
+  }
+
+  function insertNewAnswer(quizAnswers, currentCount) {
+    $('#jsRadioWrapper').fadeOut();
+    setTimeout(function() {
+    $('#jsRadioWrapper').empty().append(quizAnswers).fadeIn(); 
+    console.log('insertNewAnswer ran');
+    updateCount(currentCount);
+  }, 350);
+  }
+
+  function handleNextButtonClick(currentCount) {
+    $('#jsFormFooter').off().on('click', '.jsNextQuestionButton', event => {
+      event.preventDefault();
+      $('.jsCorrectChoice').removeClass('correct-answer');
+      $('.jsCorrectChoice i').remove();
+      $('.jsAnswerDiv i').remove();
+      $('.jsAnswerDiv').removeClass('clicked-input');
+      console.log('handleNextButtonClick() ran');
+      displayEndQuiz(currentCount);
+      renderQuestion(currentCount);
+      resetSuccess();
+      resetFailure();
+      enableInputs();
+      disableNext();
+      });
+    }
+
+  function handleSubmitAnswerClick(currentCount) {
+    $('#radio-selection').off().on('click', '.jsSubmitAnswerButton', event => {
+      event.preventDefault();
+      console.log('handleSubmitAnswerClick ran');
+      let answerChoice = $('input[name=answer]:checked').val(); 
+      handleAnswerChoice(answerChoice);
+      getCurrentScore(score);
+      disableInputs();
+      disableSubmit();
+      changeNextText(currentCount);
+      enableNext();
+    });
+  };
+
+  function handleAnswerChoice(answerChoice) {
+    if(answerChoice === "correct") {
+        score.push(1);
+        let correct = 'true';
+        handleCorrectAnswer(correct);
+      } 
+        else{
+          let incorrect = 'true';
+          handleIncorrectAnswer(incorrect);
+        }
+  }
+
+  function enableInputs() {
+    $('input').prop('disabled', false);
+    $('input').prop('checked', false);
+  }
+
+  function disableInputs() {
+    $('input').prop('disabled', true);
+  }
+
+  function inputClicked() {
+    $('fieldset').on('click', '.jsChoices', event => {
+      enableSubmit();
+    });
+  } 
+
+  function selectedAnswers() {
+
+
+  function selectedAnswerSub1() {
+    $('.choices').on('click', '.jsAnswerDiv1', event => {
+      $('.jsAnswerDiv1').addClass('clicked-input');
+      $('.jsAnswerDiv2').removeClass('clicked-input');
+      $('.jsAnswerDiv3').removeClass('clicked-input');
+      $('.jsAnswerDiv4').removeClass('clicked-input');
+    });
+  }
+
+  function selectedAnswerSub2() {
+    $('.choices').on('click', '.jsAnswerDiv2', event => {
+      $('.jsAnswerDiv2').addClass('clicked-input');
+      $('.jsAnswerDiv1').removeClass('clicked-input');
+      $('.jsAnswerDiv3').removeClass('clicked-input');
+      $('.jsAnswerDiv4').removeClass('clicked-input');
+    });
+  }
+
+  function selectedAnswerSub3() {
+    $('.choices').on('click', '.jsAnswerDiv3', event => {
+      $('.jsAnswerDiv3').addClass('clicked-input');
+      $('.jsAnswerDiv2').removeClass('clicked-input');
+      $('.jsAnswerDiv1').removeClass('clicked-input');
+      $('.jsAnswerDiv4').removeClass('clicked-input');
+    });
+  }
+
+  function selectedAnswerSub4() {
+    $('.choices').on('click', '.jsAnswerDiv4', event => {
+      $('.jsAnswerDiv4').addClass('clicked-input');
+      $('.jsAnswerDiv3').removeClass('clicked-input');
+      $('.jsAnswerDiv2').removeClass('clicked-input');
+      $('.jsAnswerDiv1').removeClass('clicked-input');
+    });
+  }
+
+    selectedAnswerSub1();
+    selectedAnswerSub2();
+    selectedAnswerSub3();
+    selectedAnswerSub4();
+}
+
+  function enableSubmit() {
+    $('.jsSubmitAnswerButton').removeClass('disabled').addClass('button').removeClass('disabled:hover').addClass('button:hover').prop('disabled', false);
+  }
+
+  function disableSubmit() {
+    $('.jsSubmitAnswerButton').addClass('disabled').addClass('disabled:hover').prop('disabled', true); 
+  }
+
+  function enableNext() {
+    $('.jsNextQuestionButton').removeClass('disabled').addClass('button').removeClass('disabled:hover').addClass('button:hover').prop('disabled', false);
+  }
+
+  function disableNext() {
+    $('.jsNextQuestionButton').addClass('disabled').addClass('disabled:hover').prop('disabled', true);
+  }
+
+  function handleCorrectAnswer() {
+    const checkMark1 =`<i class="fa fa-check" aria-hidden="true"></i>`;
+    $('.jsCorrectChoice').addClass('correct-answer');
+    $('.jsCorrectChoice').append(checkMark1);
+    console.log('handleCorrectAnswer ran');
+  }
+
+  function handleIncorrectAnswer() {
+    const ban = `<i class="fa fa-ban" aria-hidden="true"></i>`;
+    const checkMark2 =`<i class="fa fa-check" aria-hidden="true"></i>`;
+    $('.jsCorrectChoice').addClass('correct-answer');
+    $('.jsCorrectChoice').append(checkMark2);
+    console.log('handleIncorrectAnswer ran');
+    if($('.jsAnswerDiv1').hasClass('clicked-input')) {
+      $('.jsAnswerDiv1').append(ban);
+    }
+
+    if($('.jsAnswerDiv2').hasClass('clicked-input')) {
+      $('.jsAnswerDiv2').append(ban);
+    }
+
+    if($('.jsAnswerDiv3').hasClass('clicked-input')) {
+      $('.jsAnswerDiv3').append(ban);
+    }
+
+    if($('.jsAnswerDiv4').hasClass('clicked-input')) {
+      $('.jsAnswerDiv4').append(ban);
+    }
+  }
+
+  function resetSuccess() {
+    $('.jsCorrectMessage').css({"display":"none"});
+    }
+
+  function resetFailure() {
+    $('.jsFailureMessage').css({"display":"none"});
+  }
+
+  function getCurrentScore() {
+    console.log('getCurrentScore ran');
+    let currentScore = score.reduce(function(sum, value) {
+    return sum + value;
+  }, 0); 
+  $('#jsCurrentScore').empty().append(currentScore +` / 10`);
+  getFinalScore(currentScore);
+  }
+
+  function initializeScore() {
+  $('#jsCurrentScore').empty().append(`0 / 10`);
+  console.log('score initialized');
+  }
+
+  function initializeCount() {
+    $('#jsCurrentQuestion').empty().append(`1 / 10`);
+    console.log('count initialized');
+    let currentCount = 0;
+    renderQuestion(currentCount);
+  }
+
+  function updateCount(currentCount) {
+    count.push(1); 
+    $('#jsCurrentQuestion').empty().append(currentCount + `/ 10`); 
+    console.log('updateCount ran');
+    currentCount = count.reduce(function(sum, value) {
+    return sum + value;
+  }, 0); 
+      handleNextButtonClick(currentCount);
+      handleSubmitAnswerClick(currentCount);
+      updateQuestionTracker(currentCount);
+    };
+
+  function updateCountAfterReset(currentCount) {
+      renderQuestion(currentCount);
+      handleNextButtonClick(currentCount);
+      handleSubmitAnswerClick(currentCount);
+      updateQuestionTracker(currentCount);
+    };
+
+
+  function updateQuestionTracker(currentCount) {
+    $('#jsCurrentQuestion').empty().append(currentCount);
+  }
+
+  function displayEndQuiz(currentCount) {
+    if(currentCount === 10) {
+    $('.question-answer-section').fadeOut();
+    $('.finished-section').fadeIn();
+  }
+  }
+
+  function getFinalScore(currentScore) {
+    $('#jsFinalScore').empty().append(currentScore+"/ 10");
+  }
+
+  function resetScoreCount(currentCount) {
+    count.splice(0, count.length);
+    score.splice(0, score.length);
+    currentCount = 0;
+    getCurrentScore(score);
+    updateCountAfterReset(currentCount);
+    changeNextText(currentCount);
+  }
+
+  function changeNextText(currentCount) {
+    if(currentCount === 10){
+      $('span.next').text('Finish');
+    } else {
+      $('span.next').text('Next');
+    }
+    console.log('changedNextText ran');
+    console.log(currentCount);
+  }
+
+  function restartQuiz(currentCount) {
+    $('.ending-statement-container').off().on('click', '.jsRestartButton', event => {
+      $('.finished-section').fadeOut();
+      resetScoreCount(currentCount);
+      console.log(score);
+      setTimeout(function() {
+      $('.question-answer-section').fadeIn();
+    }, 200);
+    });
+  }
+
+  
+  function handleQuiz() {
+  shuffleQuestions();
+  handleStartClicks();
+  initializeCount();
+  initializeScore();
+  }
+  
+  $(handleQuiz);
+  
---
+++ questions.js
@@ -0,0 +1,95 @@
+
+
+const questionsList =[{
+
+    question: "In 'POTC1', Captain Jack Sparrow is first seen entering Port Royal on a small sinking boat. Who did he steal this boat from?",
+    correctAnswer:"Anna-Maria",
+    wrongAnswer1: "Lord Cutler Beckett",
+    wrongAnswer2: "Will Turner",
+    wrongAnswer3: "Governor Swann"},
+  
+    {question: "Whose line is this 'Ello Popet'?",
+    correctAnswer:"Pintel",
+    wrongAnswer1: "Bo'sun",
+    wrongAnswer2: "Captain Barbossa",
+    wrongAnswer3: "Captain Jack Sparrow"},
+   
+    {question: "What are the movies based on?",
+    correctAnswer:"A ride at Disneyland",
+    wrongAnswer1: "A video game",
+    wrongAnswer2: "An old sea tale",
+    wrongAnswer3: "A book"},
+  
+    {question: "What port did Captain Jack Sparrow sack without firing a single shot?",
+    correctAnswer:"Nassau Port",
+    wrongAnswer1: "Port Royal",
+    wrongAnswer2: "Tortuga",
+    wrongAnswer3: "Port Mason"},
+  
+    {question: "In POTC1, When Elizabeth was walking the plank, who said 'too long!' and stomped on the edge of the plank so she fell off?",
+    correctAnswer:"Captain Barbossa",
+    wrongAnswer1: "Captain Jack",
+    wrongAnswer2: "Ragetti",
+    wrongAnswer3: "Bo'sun"},
+  
+    {question: "Who says 'Aye! The compass doesn't point north! But we're not trying to find north are we?'",
+    correctAnswer:"Joshamee Gibbs",
+    wrongAnswer1: "Will Turner",
+    wrongAnswer2: "Captain Jack Sparrow",
+    wrongAnswer3: "Commodore Norrington"},
+  
+    {question: "How many Pirates of The Caribbean movies are there?",
+    correctAnswer:"Not enough",
+    wrongAnswer1: "Three",
+    wrongAnswer2: "6",
+    wrongAnswer3: "One"},
+  
+    {question: "What does Jack fall into after he retrieves the key?",
+    correctAnswer:"An empty grave",
+    wrongAnswer1: "A well",
+    wrongAnswer2: "A pit of animal manure",
+    wrongAnswer3: "The ocean"},
+   
+    {question: "Who's line was 'Bring me that horizon.'",
+    correctAnswer:"Jack Sparrow",
+    wrongAnswer1: "Will Turner",
+    wrongAnswer2: "Gibbs",
+    wrongAnswer3: "Pintel"},
+  
+    {question: "What nationality are the sailors that find Captain Jack's hat in POTC2?",
+    correctAnswer:"Italian",
+    wrongAnswer1: "Hungarian",
+    wrongAnswer2: "Chinese",
+    wrongAnswer3: "Greek"},
+
+    {question: "What is captain Jack Sparrow's favorite drink?",
+    correctAnswer:"Rum",
+    wrongAnswer1: "Beer",
+    wrongAnswer2: "Brandy",
+    wrongAnswer3: "Wine"},
+
+    {question: "What is POTC4 narrowly based on?",
+    correctAnswer:"A Novel",
+    wrongAnswer1: "A video game",
+    wrongAnswer2: "A toy",
+    wrongAnswer3: "A play"},
+
+    {question: "What type of animal is the pet that Jack Sparrow hates?",
+    correctAnswer:"Monkey",
+    wrongAnswer1: "Bird",
+    wrongAnswer2: "Snake",
+    wrongAnswer3: "Dolphin"},
+
+    {question: "What did Jack need from a mermaid at the Fountain of Youth?",
+    correctAnswer:"A Tear",
+    wrongAnswer1: "A tooth",
+    wrongAnswer2: "A Kiss",
+    wrongAnswer3: "A Hug"},
+
+    {question: "What is Jack's favorite ship called?",
+    correctAnswer:"The Black Pearl",
+    wrongAnswer1: "The Flying Dutchman",
+    wrongAnswer2: "The Drunken Pirate",
+    wrongAnswer3: "The Matador"}
+    
+    ];
